82a02b2b5db3c6bc17b9265c088a4e16f3720b27
navbharti <nav.bharti@gmail.com>
2020-02-25 22:11:52 +0530

    Insertion Sort program format

---
+++ DSJava/src/insertionsort/InsertionSortConsole.java
@@ -0,0 +1,27 @@
+package insertionsort;
+
+public class InsertionSortConsole {
+
+	public static void main(String[] args) {
+		
+		//Read value of n
+		
+		//Create an array with size n
+		
+		//Read n elements in the array
+		
+		//display the elements of the array before sorting
+		
+		//call insertionSort() method
+		
+		//display the elements of the array after sorting.
+
+	}
+
+	//Write body code for insertionSort() method
+	public static void insertionSort(int arr[], int n) {
+		//your code for Insertion Sorting goes here.
+		
+		
+	}
+}
