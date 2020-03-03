1f0ee1ac0c5d3f3762a9d212b682b9229199a9b5
navbharti <nav.bharti@gmail.com>
2020-03-03 21:50:58 +0530

    Skeleton of SLL, DLL, Circular SLL and Circular DLL

---
+++ DSJava/src/linearDS/CircularSLL.java
@@ -0,0 +1,105 @@
+package linearDS;
+
+public class CircularSLL {
+
+	Node head;
+	
+	/*
+	 * method to insertAtBegin
+	 */
+	public void insertAtBegin(DNode node) {
+		
+	}
+	
+	/*
+	 * method to insertAtEnd
+	 */
+	public void insertAtEnd(DNode node) {
+		
+	}
+	
+	/*
+	 * method to insertByPosition
+	 */
+	public void insertByPosition(DNode node) {
+		
+	}
+	
+	/*
+	 * method to insertByData
+	 */
+	public void insertByData(DNode node) {
+		
+	}
+	
+	/*
+	 * method to deleteFromBegin
+	 */
+	public DNode deleteFromBegin() {
+		
+		return null;
+	}
+	
+	/*
+	 * method to deleteFromEnd
+	 */
+	public DNode deleteFromEnd() {
+		
+		return null;
+	}
+	
+	/*
+	 * method to deleteByPosition
+	 */
+	public DNode deleteByPosition() {
+		
+		return null;
+	}
+	
+	/*
+	 * method to deleteByData
+	 */
+	public DNode deleteByData() {
+		
+		return null;
+	}
+	
+	/*
+	 * method to countNodes
+	 */
+	public int countNodes() {
+		
+		return 0;
+	}
+	
+	/*
+	 * method to traverse
+	 */
+	public void traverse() {
+		
+	}
+	
+	/*
+	 * method to search
+	 */
+	public DNode search(int data) {
+		
+		return null;
+	}
+	
+	/*
+	 * method to update data of a given position
+	 */
+	public boolean updateByPosition(int newData) {
+		
+		return false;
+	}
+	
+	/*
+	 * method to update data of a given oldData value
+	 */
+	public boolean updateByData(int oldData, int newData) {
+		
+		return false;
+	}
+}
---
+++ DSJava/src/linearDS/DNode.java
@@ -0,0 +1,12 @@
+package linearDS;
+
+public class DNode {
+	//data part
+	int data;
+	
+	//link for previous node
+	DNode prev;
+	
+	//link for next node
+	DNode next;
+}
