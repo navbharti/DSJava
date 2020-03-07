ecaa7ced4cbe82d7aca75023a1fabc90c6ef57c2
navbharti <nav.bharti@gmail.com>
2020-03-07 10:46:28 +0530

    complete DoublyLinkedList

--- DSJava/src/linearDS/DoublyLinkedList.java
+++ DSJava/src/linearDS/DoublyLinkedList.java
@@ -60,6 +60,70 @@ public class DoublyLinkedList {
 				System.out.println("\nTotal Node: "+list.countNodes());
 				//Output: Total Node: 5
 				
+				//Delete the first node
+				node = list.deleteFromBegin();
+				System.out.println("\nDeleted: "+node.data);
+				list.traverse();
+				//Output: Deleted: 20
+
+				//null<->500<->700<->10<->100<->null
+				
+				//Delete the node with position=3
+				position=3;
+				node=list.deleteByPosition(position);
+				System.out.println("\nDeleted: "+node.data);
+				list.traverse();
+				//Output: Deleted: 10
+
+				//null<->500<->700<->100<->null
+				
+				//Delete the last node
+				node = list.deleteFromEnd();
+				System.out.println("\nDeleted: "+node.data);
+				list.traverse();
+				//Output: Deleted: 100
+
+				//null<->500<->700<->null
+				
+				node = new DNode();
+				node.data=15;
+				list.insertAtBegin(node);
+				
+				node = new DNode();
+				node.data=155;
+				list.insertAtEnd(node);
+				list.traverse();
+				//Output: null<->15<->500<->700<->155<->null
+				
+				//Delete the node with data=700
+				data=700;
+				node = list.deleteByData(data);
+				System.out.println("\nDeleted: "+node.data);
+				list.traverse();
+				//Output: Deleted: 700
+
+				//null<->15<->500<->155<->null
+				
+				//update begin node data=15
+				list.updateBegin(1555);
+				list.traverse();
+				//Output: null<->1555<->500<->155<->null
+				
+				//update end node data=23
+				list.updateEnd(23);
+				list.traverse();
+				//Output: null<->1555<->500<->23<->null
+				
+				//updateByPosition position=2
+				list.updateByPosition(17, 2);
+				list.traverse();
+				//Output: null<->1555<->17<->23<->null
+				
+				//updateByData oldData=17 and newData=170
+				list.updateByData(17, 170);
+				list.traverse();
+				//Output: null<->1555<->170<->23<->null
+				
 			}
 			
 			/***************************************
@@ -70,14 +134,11 @@ public class DoublyLinkedList {
 			 * 1. method to insert a node at the beginning
 			 */
 			public void insertAtBegin(DNode node) {
-				
-				//check for empty case, if yes assign node to the head and return
 				if(this.isEmpty()==true) {
 					head=node;
 					return;
 				}
 
-				//otherwise change the references of node and head
 				node.next=head;
 				head.prev=node;
 				head=node;
@@ -87,8 +148,6 @@ public class DoublyLinkedList {
 			 * 2. method to insert a node at the end
 			 */
 			public void insertAtEnd(DNode node) {
-				
-				//check for empty case, if yes assign node to the head and return
 				if(this.isEmpty()==true) {
 					head=node;
 					return;
@@ -104,33 +163,15 @@ public class DoublyLinkedList {
 			 * 3. method to insert a node by position
 			 */
 			public void insertByPosition(DNode node, int position) {
-				
-				//Check for empty case, if yes assign node to the head and return
-				if(this.isEmpty()==true) {
-					head=node;
-					return;
-				}
-				
-				//Otherwise, Initialize temp node to head node
 				DNode temp=head;
-				
-				//Initialize count to zero for node position
 				int count=0;
-				
-				//Repeat till temp.next is not null
 				while(temp.next!=null) {
-					//For each repetition increase count with 1
 					count++;
-					
-					//Break the repetition if count reached just before position
 					if(count==position-1)
 						break;
-					
-					//For each repetition temp points to next node 
 					temp=temp.next;
 				}
 				
-				//change the references
 				node.prev=temp;
 				node.next=temp.next;
 				temp.next.prev=node;
@@ -141,25 +182,13 @@ public class DoublyLinkedList {
 			 * 4. method to insert a node by data key value
 			 */
 			public void insertByData(DNode node, int data) {
-				
-				//check for empty case, if yes assign node to the head and return
-				if(this.isEmpty()==true) {
-					head=node;
-					return;
-				}
-				
-				//Otherwise, Initialize temp node to head node
 				DNode temp=head;
-				
-				//Repeat till temp.next is not null
 				while(temp.next!=null) {
-					//Break the repetition if temp.data is matched with data
 					if(temp.data==data)
 						break;
 					temp=temp.next;
 				}
 				
-				//change the references
 				node.prev=temp;
 				node.next=temp.next;
 				temp.next.prev=node;
@@ -174,102 +203,97 @@ public class DoublyLinkedList {
 			 * 1. Delete a node from beginning
 			 */
 			public DNode deleteFromBegin() {
-				//check for empty case, if yes print Empty message and return null
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return null;
+				}
+				DNode temp=head;
+				head.next.prev=null;
+				head=head.next;
+				temp.next=null;
 				
-				
-				//Initialize temp node to head node
-				
-				//Change the reference for temp and head
-				
-				//Return the reference of the deleted node
-				return null;
+				return temp;
 			}
 			
 			/*
 			 * 2. Delete a node from end
 			 */
 			public DNode deleteFromEnd() {
-				//Check for Empty case, if yes print Empty message and return null
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return null;
+				}
 				
+				DNode temp=head;
+				while(temp.next!=null) {
+					temp = temp.next;
+				}
 				
-				//Initialize temp node to head node
+				temp.prev.next=null;
+				temp.prev=null;
 				
-				//Repeat till last nose
-				
-				
-				//change the reference 
-				
-				//Return the reference of deleted node
-				return null;
+				return temp;
 			}
 			
 			/*
 			 * 3. Delete a node by position
 			 */
 			public DNode deleteByPosition(int position) {
-				//Check for Empty case, if yes print Empty message return null
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return null;
+				}
+				int count=0;
+				DNode temp=head;
+				while(temp.next!=null) {
+					count++;
+					if(count==position)
+						break;
+					temp = temp.next;
+				}
 				
+				temp.next.prev=temp.prev;
+				temp.prev.next=temp.next;
+				temp.prev=null;
+				temp.next=null;
 				
-				//Otherwise, Initialize count=0 and temp node to head node
-				
-				//Repeat till last node
-				
-					//For each repetition increase count with 1
-					
-					//stop repetition if count is equal to position
-					
-					//For each repetition point temp node to the next node
-					
-				
-				//Change the reference to delete the node
-				
-				//Return the reference of the deleted node
-				return null;
+				return temp;
 			}
-			
 			/*
 			 * 4. Delete a node by data key value
 			 */
 			public DNode deleteByData(int data) {
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return null;
+				}
+				DNode temp=head;
+				while(temp.next!=null) {
+					if(temp.data==data)
+						break;
+					temp = temp.next;
+				}
 				
-				//Check for Empty case, if yes print the Empty message and return 
+				temp.next.prev=temp.prev;
+				temp.prev.next=temp.next;
+				temp.prev=null;
+				temp.next=null;
 				
-				
-				//Otherwise, Initialize temp node to head node
-				
-				//Repeat till last node
-					
-					//For each repetition chech temp.data matched with data
-					
-					//For each repetition point temp to the next node
-					
-				
-				//Change references to delete node
-				
-				//Return the deleted node
-				return null;
+				return temp;
 			}
 			
 			/*
 			 * Traversal of nodes in the list
 			 */
 			public void traverse() {
-				
-				//Check for Empty case, if yes print Empty message return back
 				if(this.isEmpty()==true) {
 					System.out.println("List is Empty!!!");
 					return;
 				}
-				
-				//Otherwise, Initialize temp node to head node
 				DNode temp=head;
 				System.out.println();
 				System.out.print("null<->");
-				
-				//Repeat till last node
 				while(temp!=null) {
-					
-					//For each repetition print temp.data and point temp to next node
 					System.out.print(temp.data+"<->");
 					temp=temp.next;
 				}
@@ -279,25 +303,12 @@ public class DoublyLinkedList {
 			 * Reverse Traversal of nodes in the list
 			 */
 			public void reverseTraversal() {
-				//Check for Empty case, if yes print Empty message and return back
-				if(this.isEmpty()==true) {
-					System.out.println("List is Empty!!!");
-					return;
-				}
-				
-				//Otherwise, Initialize temp node to head node
 				DNode temp = head;
-				
-				//Repeat till the last node
 				while(temp.next!=null)
 					temp = temp.next;
 				System.out.println();
 				System.out.print("null<->");
-				
-				//Repeat till temp is not null
 				while(temp!=null) {
-					
-					//For each repetition print temp.data and go to previous node
 					System.out.print(temp.data+"<->");
 					temp=temp.prev;
 				}
@@ -308,27 +319,12 @@ public class DoublyLinkedList {
 			 * count the nodes in the list
 			 */
 			public int countNodes() {
-				//Check for Empty case, if yes print Empty message and return -1
-				if(this.isEmpty()==true) {
-					System.out.println("List is Empty!!!");
-					return -1;
-				}
-				
-				//Initialize temp node to head node
 				DNode temp=head;
-				
-				//Initialize count to zero for node's current position
 				int count=0;
-				
-				//Repeat till the last node
 				while(temp!=null) {
-					
-					//For each node increase count with 1, and point temp to next node
 					count++;
 					temp = temp.next;
 				}
-				
-				//return the count value
 				return count;
 			}
 			
@@ -340,27 +336,27 @@ public class DoublyLinkedList {
 			 * 1. update the data of first node
 			 */
 			public boolean updateBegin(int data) {
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return false;
+				}
 				
-				//Check for Empty case, if yes print Empty message and return false
-				
-				
-				//Otherwise, update the data part of first not to given data value
-				
+				head.data=data;
 				return true;
 			}
 			/*
 			 * 2. update the data of last node
 			 */
 			public boolean updateEnd(int data) {
-				//Check for Empty case, if yes print Empty message and return false
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return false;
+				}
 				
-				
-				//Initialize temp node to head node
-				
-				//Repeat till the last node
-				
-				//Change the data part of the last node to given data value
-				
+				DNode temp=head;
+				while(temp.next!=null)
+					temp = temp.next;
+				temp.data=data;
 				return true;
 			}
 			
@@ -368,27 +364,54 @@ public class DoublyLinkedList {
 			 * 3. update the data by position
 			 */
 			public boolean updateByPosition(int data, int position) {
-				//Check for Empty case, if yes print Empty message and retrurn false
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return false;
+				}
+				DNode temp = head;
+				int count = 0;
+				boolean flag=false;
 				
+				while(temp.next!=null) {
+					count++;
+					if(count==position) {
+						flag = true;
+						break;
+						
+					}
+					temp = temp.next;
+				}
 				
-				//Otherwise, Initialize temp node to head node
+				if(flag==true) {
+					temp.data=data;
+					return true;
+				}
 				
-				//Initialize count with zero to point current position of node
-				
-				//Keep flag to identify whether node if found or not
-				
-				//Repeat till the last node
-					//For each repetition increase count with 1
-					
-					//For each repetition check count is equal to given position
-					
 				return false;
 			}
 			/*
 			 * 4. update the data by key value
 			 */
 			public boolean updateByData(int oldData, int newData) {
+				if(this.isEmpty()==true) {
+					System.out.println("List is Empty!!!");
+					return false;
+				}
 				
+				DNode temp = head;
+				boolean flag=false;
+				while(temp.next!=null) {
+					if(temp.data==oldData) {
+						flag = true;
+						break;
+					}
+					temp = temp.next;
+				}
+				
+				if(flag==true) {
+					temp.data = newData;
+					return true;
+				}
 				return false;
 			}
 			
