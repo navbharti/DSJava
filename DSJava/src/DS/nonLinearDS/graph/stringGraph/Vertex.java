4531086eae4c57c02ccdae7f57334906b3ae5817
navbharti <nav.bharti@gmail.com>
2020-03-29 17:30:14 +0530

    generic Graph

---
+++ DSJava/src/DS/nonLinearDS/graph/stringGraph/Vertex.java
@@ -0,0 +1,10 @@
+package DS.nonLinearDS.graph.stringGraph;
+
+public class Vertex {
+	    String label;
+	    Vertex(String label) {
+	        this.label = label;
+	    }
+	 
+	    // equals and hashCode
+	}
